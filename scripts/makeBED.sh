38c6f8d6b60a8d3c8cb1b56e6bf57defe5d35539
Francisco J. Villena González <francis.villena@hotmail.es>
2024-05-24 10:48:47 +0200

    Add makeBED.sh

---
+++ scripts/makeBED.sh
@@ -0,0 +1,10 @@
+chr=$1
+stage=$2
+
+# Capture dimensions of chromosomes from haplotypes
+cut -f1,2 out/${stage}HACk/*.fai res/$chr.fa.fai | \
+# Obtain the maximum dimension
+sort | awk '$2 > maxvals[$1] {lines[$1]=$0; maxvals[$1]=$2} END { for (tag in lines) print lines[tag] }' | \
+# Generate a BED file with the maximum chromosome length 
+awk 'OFS=FS="\t" {print $1, "1", $2, "100.0", "100.0"}' > out/${stage}HACk/laser.simple.bed
+
