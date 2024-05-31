5dc806662a029696455924c1655beb1ce1f8bbee
Francisco J. Villena González <francis.villena@hotmail.es>
2024-05-31 12:27:14 +0200

    Updated pipeline to work with whole genomes

--- workflow/scripts/makeBED.sh
+++ workflow/scripts/makeBED.sh
@@ -1,10 +1,9 @@
-chr=$1
-stage=$2
+stage=$1
+genome_file=$2
 
 # Capture dimensions of chromosomes from haplotypes
-cut -f1,2 results/${stage}HACk/*.fai res/$chr.fa.fai | \
+cut -f1,2 results/${stage}HACk/*.fai resources/genome/${genome_file}.fai | \
 # Obtain the maximum dimension
 sort | awk '$2 > maxvals[$1] {lines[$1]=$0; maxvals[$1]=$2} END { for (tag in lines) print lines[tag] }' | \
 # Generate a BED file with the maximum chromosome length 
 awk 'OFS=FS="\t" {print $1, "1", $2, "100.0", "100.0"}' > results/${stage}HACk/laser.simple.bed
-
